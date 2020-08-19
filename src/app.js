dbd38abbcf1338bf497aa93369e62310c374845e
Pedro Leal <pedrohb88@hotmail.com>
2020-08-19 19:57:38 -0300

    aaaaaaaaaaa

--- src/app.js
+++ src/app.js
@@ -303,6 +303,6 @@ if(process.env.NODE_ENV === 'production'){
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
     });
-}git 
+}
 
 module.exports = app;
