160116e2359e29c2434678dec602c8b446a1c313
Pedro Leal <pedrohb88@hotmail.com>
2021-02-13 22:40:13 -0300

    remove ativos and suspeitos charts

--- client/src/ChartPanel.js
+++ client/src/ChartPanel.js
@@ -7,10 +7,10 @@ const ChartPanel = ({data}) => {
     return (
         <div className="chartPanel">
             <Chart title='Casos Confirmados' yTitle='Acumulado de Casos' dataPoints={data.confirmados.dataPoints}/>
-            <Chart title='Casos Confirmados e Ativos' yTitle='Acumulado de Casos' dataPoints={data.ativos.dataPoints}/>
+            {/*}<Chart title='Casos Confirmados e Ativos' yTitle='Acumulado de Casos' dataPoints={data.ativos.dataPoints}/>*/}
             <Chart title='Óbitos' yTitle='Acumulado de Casos' dataPoints={data.obitos.dataPoints}/>
             <Chart title='Casos Curados' yTitle='Acumulado de Casos' dataPoints={data.cura.dataPoints}/>
-            <Chart title='Casos Suspeitos' yTitle='Acumulado de Casos' dataPoints={data.suspeitos.dataPoints}/>
+            {/*<Chart title='Casos Suspeitos' yTitle='Acumulado de Casos' dataPoints={data.suspeitos.dataPoints}/>*/}
             <Chart title='Casos Descartados' yTitle='Acumulado de Casos' dataPoints={data.descartados.dataPoints}/>
             <Chart title='Casos Notificados' yTitle='Acumulado de Casos' dataPoints={data.notificados.dataPoints}/>
         </div>
